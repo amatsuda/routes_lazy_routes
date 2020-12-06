8ec7a38b6dd86aa5e789221511304762dbe0d0c8
Akira Matsuda <ronnie@dio.jp>
2020-12-06 22:16:46 +0900

    Something that wraps the routes reloader to get to the routes another day

--- lib/routes_lazy_routes.rb
+++ lib/routes_lazy_routes.rb
@@ -2,6 +2,7 @@
 
 require_relative 'routes_lazy_routes/version'
 require_relative 'routes_lazy_routes/lazy_routes_middleware'
+require_relative 'routes_lazy_routes/routes_reloader_wrapper'
 
 module RoutesLazyRoutes
   class Error < StandardError; end
---
+++ lib/routes_lazy_routes/routes_reloader_wrapper.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module RoutesLazyRoutes
+  class RoutesReloaderWrapper
+    delegate :paths, :eager_load=, :updated?, :route_sets, to: :@original_routes_reloader
+
+    def initialize(original_routes_reloader)
+      @original_routes_reloader = original_routes_reloader
+
+      Rails.application.config.middleware.use LazyRoutesMiddleware, original_routes_reloader
+    end
+
+    def execute
+      # pretty vacant
+    end
+  end
+end
