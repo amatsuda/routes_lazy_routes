feebbdf20e0c5ad4c5fe553ee89af586fc0153ee
Akira Matsuda <ronnie@dio.jp>
2020-12-06 23:19:16 +0900

    The worst thing that'll happen when you called RoutesReloader.new

--- lib/routes_lazy_routes.rb
+++ lib/routes_lazy_routes.rb
@@ -5,6 +5,16 @@ require_relative 'routes_lazy_routes/lazy_routes_middleware'
 require_relative 'routes_lazy_routes/routes_reloader_wrapper'
 
 module RoutesLazyRoutes
-  class Error < StandardError; end
-  # Your code goes here...
+  class << self
+    # The root of evil
+    def arise!
+      Rails::Application::RoutesReloader.class_eval do
+        class << self
+          def new
+            RoutesLazyRoutes::RoutesReloaderWrapper.new super
+          end
+        end
+      end
+    end
+  end
 end
