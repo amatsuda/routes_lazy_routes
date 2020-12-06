e3d6ced156175dd7772808b1ffe096032e707666
Akira Matsuda <ronnie@dio.jp>
2020-12-06 23:54:12 +0900

    A Rails app for the tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,16 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "routes_lazy_routes"
 
 require "minitest/autorun"
+
+ENV['RAILS_ENV'] ||= 'test'
+
+module RoutesLazyRoutesTestApp
+  Application = Class.new(Rails::Application) do
+    config.eager_load = false
+    config.active_support.deprecation = :log
+    config.root = __dir__
+    config.secret_key_base = 'Routes, lazy routes.' * 4
+  end
+end
+
+RoutesLazyRoutesTestApp::Application.initialize!
