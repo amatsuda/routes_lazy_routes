8321d82ee747fc5ff084d6d7f5c4a5642328c83c
Akira Matsuda <ronnie@dio.jp>
2020-12-08 12:09:52 +0900

    It's symbolic, of course

--- lib/routes_lazy_routes/railtie.rb
+++ lib/routes_lazy_routes/railtie.rb
@@ -4,7 +4,7 @@ require_relative 'application'
 
 module RoutesLazyRoutes
   class Railtie < ::Rails::Railtie
-    initializer 'routes_lazy_routes', before: :add_routing_paths do
+    initializer :routes_lazy_routes, before: :add_routing_paths do
       RoutesLazyRoutes.arise!
 
       Rails.application.config.middleware.use LazyRoutesMiddleware
